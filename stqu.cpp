d1b33a68ac52ef98970f1924e4cfc9a79f7e69aa
RiyaJaipuria <riya.jaipuria@gmail.com>
2020-02-22 00:53:23 -0800

    stack and queue

---
+++ stqu.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<functional>
+#include<vector>
+#include<list>
+#include<stack>
+#include<queue>
+using namespace std;
+//stl container adaptors: stck,queue and priority queue
+
+int main()
+{
+	stack<int ,vector<int>> cst;
+	cstk.push(100);
+	cstk.push(200);
+	cstk.push(300);
+	while(!cstk.empty())
+	{
+		cout<<cstk.top()<<endl;
+		cstk.pop();
+		
+	}
+
+
+	return 0;
+}
+
